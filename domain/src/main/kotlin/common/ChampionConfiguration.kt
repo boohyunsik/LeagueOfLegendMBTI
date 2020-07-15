b59a7fba4d94ef780421ba0582b66d854740461c
boohyunsik <bhs9194@nate.com>
2020-07-16 03:26:08 +0900

    wip: champion constants

---
+++ domain/src/main/kotlin/common/ChampionConfiguration.kt
@@ -0,0 +1,14 @@
+package common
+
+enum class ChampionConfiguration(key: Int, name: String) {
+    AATROX(266, "Aatrox"),
+    AHRI(103, "Ahri"),
+    AKALI(84, "Akali"),
+    ALISTAR(12, "Alistar"),
+    AMUMU(32, "Amumu"),
+    ANIVIA(34, "Anivia"),
+    ANNIE(1, "Annie"),
+    APHELIOS(523, "Aphelios")
+    // TODO : fill this!
+
+}
